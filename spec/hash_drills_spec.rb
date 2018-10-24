e08a1ba8408e5cef03defbfdd0b8bd5b843612df
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-10-24 14:12:12 -0500

    Starts adding solution and tests

--- spec/hash_drills_spec.rb
+++ spec/hash_drills_spec.rb
@@ -37,9 +37,9 @@ describe 'vegetarian_ingredients' do
   end
 
   it 'returns a hash of vegetarian ingredients' do
-    expect(vegetarian_ingredients[:sweets]).to include("soda", "candy", "potato chips")  
-    expect(vegetarian_ingredients[:protein]).not_to include(:meat)  
-    expect(vegetarian_ingredients[:protein][:other]).to include("eggs", "nuts","beans")  
+    expect(vegetarian_ingredients[:sweets]).to include("soda", "candy", "potato chips")
+    expect(vegetarian_ingredients[:protein]).not_to include(:meat)
+    expect(vegetarian_ingredients[:protein][:other]).to include("eggs", "nuts","beans")
     expect(vegetarian_ingredients[:dairy]).to include("milk", "yogurt", "cheese")
     expect(vegetarian_ingredients[:fruits]).to include("bananas", "oranges", "apples", "grapes")
     expect(vegetarian_ingredients[:vegetables]).to include("cabbage", "broccoli", "tomatoes", "carrots")
@@ -58,15 +58,15 @@ describe 'ketogenic_ingredients' do
 
     it 'returns a hash of ketogenic ingredients' do
       expect(ketogenic_ingredients).not_to include(:sweets)
-      expect(ketogenic_ingredients[:protein][:meat]).to include("chicken (white)", "fish (white)", "steak (red)")  
-      expect(ketogenic_ingredients[:protein][:other]).to include("eggs", "nuts","beans")  
+      expect(ketogenic_ingredients[:protein][:meat]).to include("chicken (white)", "fish (white)", "steak (red)")
+      expect(ketogenic_ingredients[:protein][:other]).to include("eggs", "nuts","beans")
       expect(ketogenic_ingredients[:dairy]).to include("milk", "yogurt", "cheese")
       expect(ketogenic_ingredients).not_to include(:fruits)
       expect(ketogenic_ingredients[:vegetables]).to include("cabbage", "broccoli", "tomatoes", "carrots")
       expect(ketogenic_ingredients).not_to include(:grains)
     end
 end
- 
+
 describe 'mediterranean_ingredients' do
 
     it 'has a method for mediterranean ingredients' do
@@ -76,7 +76,7 @@ describe 'mediterranean_ingredients' do
     it 'returns a mediterranean list that is a hash' do
         expect(mediterranean_ingredients).to be_kind_of(Hash)
     end
-  
+
     it 'returns a hash of mediterranean ingredients' do
       expect(mediterranean_ingredients).not_to include(:sweets)
       expect(mediterranean_ingredients[:protein][:meat]).to include("chicken (white)", "fish (white)")
@@ -97,12 +97,12 @@ describe 'vegan_ingredients' do
     it 'returns a vegan list that is a hash' do
         expect(vegan_ingredients).to be_kind_of(Hash)
     end
-  
+
     it 'returns a hash of vegan ingredients' do
-      expect(vegan_ingredients[:sweets]).to include("soda", "candy", "potato chips")  
-      expect(vegan_ingredients[:protein]).not_to include(:meat)  
-      expect(vegan_ingredients[:protein][:other]).to include("nuts","beans")  
-      expect(vegan_ingredients[:protein][:other]).not_to include("eggs")  
+      expect(vegan_ingredients[:sweets]).to include("soda", "candy", "potato chips")
+      expect(vegan_ingredients[:protein]).not_to include(:meat)
+      expect(vegan_ingredients[:protein][:other]).to include("nuts","beans")
+      expect(vegan_ingredients[:protein][:other]).not_to include("eggs")
       expect(vegan_ingredients).not_to include(:dairy)
       expect(vegan_ingredients[:fruits]).to include("bananas", "oranges", "apples", "grapes")
       expect(vegan_ingredients[:vegetables]).to include("cabbage", "broccoli", "tomatoes", "carrots")
