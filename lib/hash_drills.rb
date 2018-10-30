1f0cae04ac46079b637b5bcac720f94db024e9bc
maxwellbenton <maxwellbenton@gmail.com>
2018-10-30 17:30:22 -0400

    added solution

--- lib/hash_drills.rb
+++ lib/hash_drills.rb
@@ -13,3 +13,42 @@ def shopping_list
 end
 
 # Write methods and implementation here based on README instructions and tests
+
+
+def vegetarian_ingredients
+  vegetarian_list = shopping_list
+  vegetarian_list[:protein].delete(:meat)
+  vegetarian_list[:protein][:other].shift
+  vegetarian_list
+end
+
+def ketogenic_ingredients
+  keto_list = shopping_list.select {|key, value|
+    [:vegetables, :protein, :dairy].include? key
+  }
+  keto_list[:dairy] = ["cheese"]
+  keto_list
+end
+
+def mediterranean_ingredients
+  okay_categories = [:protein, :dairy, :fruits, :vegetables, :grains]
+  med_list = shopping_list.select {|key,value|
+    okay_categories.include? key
+  }
+  med_list[:protein][:meat].delete("steak")
+  med_list[:protein][:other] = ["nuts"]
+  med_list[:dairy].delete("milk")
+  med_list
+end
+
+
+def vegan_ingredients
+  vegan_list = vegetarian_ingredients
+  vegan_list.delete(:dairy)
+  vegan_list
+end
+
+def remove_allergens(ingredients_hash)
+  ingredients_hash[:protein][:other].delete("nuts")
+  ingredients_hash
+end
