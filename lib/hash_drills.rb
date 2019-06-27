2a58bacbd15daa971b203d92a6cb36a0b3f3bfaa
github username <github email address>
2019-06-27 09:20:57 +0100

    Done.

--- lib/hash_drills.rb
+++ lib/hash_drills.rb
@@ -13,3 +13,42 @@ def shopping_list
 end
 
 # Write methods and implementation here based on README instructions and tests
+def vegetarian_ingredients
+    veggies = shopping_list
+    veggies[:protein].delete(:meat)
+    veggies[:protein][:other].shift
+    veggies
+end
+
+def ketogenic_ingredients
+    keto = shopping_list
+    keto.delete(:grains)
+    keto.delete(:sweets)
+    keto.delete(:fruits)
+    keto[:dairy].shift
+    keto[:dairy].shift
+    # keto[:protein][:other].pop
+    keto
+end
+
+def mediterranean_ingredients 
+    medi = shopping_list
+    medi.delete(:sweets)
+    medi[:dairy].shift
+    medi[:protein][:meat].pop
+    medi[:protein][:other].pop
+    medi[:protein][:other].shift
+    medi
+end
+
+def vegan_ingredients
+    vegan = vegetarian_ingredients
+    vegan.delete(:dairy)
+    vegan
+end
+
+def remove_allergens(any_ingred)
+    allergenless = any_ingred
+    allergenless[:protein][:other].delete(:nuts)
+    allergenless
+end
